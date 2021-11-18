e66675419bd060bfdb26cf003ced431cb9fbbd72
Gabriella Romeo <94238147+GMCRomeo@users.noreply.github.com>
2021-11-18 08:07:13 +0100

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,18 @@
+let weather = {
+  temp: 13,
+  humidity: 60,
+  windSpeed: 40,
+};
+console.log(weather.temp);
+console.log(weather.humidity);
+console.log(weather.windSpeed);
+
+let forecast = [
+  {
+    temp: 13,
+    humidity: 60,
+    windSpeed: 40,
+  },
+];
+console.log(forecast);
+console.log(forecast[0].temp);

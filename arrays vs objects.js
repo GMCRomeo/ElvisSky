5faff9a8af4ee04ab206b74c32d2140ea8e19a78
Gabriella Romeo <94238147+GMCRomeo@users.noreply.github.com>
2021-11-18 08:26:15 +0100

    Add files via upload

---
+++ arrays vs objects.js
@@ -0,0 +1,47 @@
+let paris = {
+  name: "Paris",
+  country: "France",
+  language: "French",
+  temperature: 18,
+  capitalCity: true,
+};
+console.log(paris.temperature);
+let sydney = {
+  name: "Sydney",
+  country: "Australia",
+  language: "English",
+  temperature: 15,
+  capitalCity: true,
+};
+console.log(sydney.temperature);
+
+let Australia = ["Sydney", "Perth", "Adelaide"];
+console.log(Australia[0]);
+console.log(Australia[1]);
+console.log(Australia[2]);
+
+let cities = [
+  {
+    name: "Rome",
+    country: "Italy",
+    language: "Italian",
+    temperature: 10,
+    capitalCity: true,
+  },
+  {
+    name: "Pisa",
+    country: "Italy",
+    language: "Italian",
+    temperature: 15,
+    capitalCity: false,
+  },
+  {
+    name: "Sydney",
+    country: "Australia",
+    language: "English",
+    temperature: 15,
+    capitalCity: true,
+  },
+];
+
+console.log(cities[0].temperature);
